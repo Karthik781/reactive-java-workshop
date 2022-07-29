cf7e0ef70583e4c8ca0c3b01079bdab770781cdd
karthik781 <karthikmsdian78@gmail.com>
2022-07-29 16:18:13 +0530

    check 123

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -9,10 +9,7 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        ReactiveSources.intNumbersFlux().subscribe( e -> {
-            e = e +1;
-            System.out.println(e);
-        }); 
+        ReactiveSources.intNumbersFlux().subscribe( e -> System.out.println(e)); 
 
         // Print all users in the ReactiveSources.userFlux stream
         ReactiveSources.userFlux().subscribe( user -> System.out.println(user));
